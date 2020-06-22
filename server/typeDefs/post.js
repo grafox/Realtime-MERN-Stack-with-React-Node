5db6e5a88e74f678a4ba270a6b09f556bfe742de
Gassan <g_fox_fts@yahoo.com>
2020-06-22 21:41:17 +0200

    client

--- server/typeDefs/post.js
+++ server/typeDefs/post.js
@@ -6,14 +6,14 @@ module.exports = gql`
         title: String!
         description: String!
     }
-    type Query {
-        totalPosts: Int!
-        allPosts: [Post!]!
-    }
     # input type
     input PostInput {
         title: String!
         description: String!
+    }
+    type Query {
+        totalPosts: Int!
+        allPosts: [Post!]!
     }
     # mutations
     type Mutation {
