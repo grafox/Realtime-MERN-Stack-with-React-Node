cfa78743c32fd8ca597fc9754c4c32d39b772543
Gassan <g_fox_fts@yahoo.com>
2020-06-27 20:20:31 +0200

    login and register

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import ApolloClient from 'apollo-boost';
 import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
+import { AuthContext } from '../context/authContext';
+import { useHistory } from 'react-router-dom';
 
 const GET_ALL_POSTS = gql`
   {
@@ -15,6 +17,19 @@ const GET_ALL_POSTS = gql`
 
 const Home = () => {
   const { data, loading, error } = useQuery(GET_ALL_POSTS);
+  const [fetchPosts, { data: posts }] = useLazyQuery(GET_ALL_POSTS);
+  // access context
+  const { state, dispatch } = useContext(AuthContext);
+  // react router
+  let history = useHistory();
+
+  const updateUserName = () => {
+    dispatch({
+      type: 'LOGGED_IN_USER',
+      payload: 'Ryan Dhungel'
+    });
+  };
+
   if (loading) return <p className="p-5">Loading...</p>;
 
   return (
@@ -33,6 +48,21 @@ const Home = () => {
           </div>
         ))}
       </div>
+      <div className="row p-5">
+        <button onClick={() => fetchPosts()} className="btn-btn-raised btn-primary">
+          Fetch posts
+        </button>
+      </div>
+      <hr />
+      {JSON.stringify(posts)}
+      <hr />
+      {JSON.stringify(state.user)}
+      <hr />
+      <button className="btn btn-primary" onClick={updateUserName}>
+        Change user name
+      </button>
+      <hr />
+      {JSON.stringify(history)}
     </div>
   );
 };
